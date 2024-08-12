59bff5b9e6383d01db3fb5d6d5167f51db8a085d
khamitovabdulhamid <khamitovabdulhamit@gmail.com>
2024-08-12 11:49:38 +0500

    hamid pro

--- task1/Event.cs
+++ task1/Event.cs
@@ -6,4 +6,5 @@ public class Event
     public string description { get; set; }
     public DateTime startTime { get; set; }
     public DateTime endTime { get; set; }
+    public DateTime Time { get; set; }
 }
--- task2/obj/Debug/net8.0/task2.AssemblyInfo.cs
+++ task2/obj/Debug/net8.0/task2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c76a17ea30d2d1112d0480aeef7aaeec7c6ced2")]
 [assembly: System.Reflection.AssemblyProductAttribute("task2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
